c865acb89e585a22cd0b8becd074803eab0c0bcd
wakenedo <a_nuner@outlook.com>
2022-02-14 21:11:23 -0300

    Home.module.scss to Global.scss
    Adapted the home.module.scss into a global file for styles, and added a few default styles for better scalability and readability of the code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import styles from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>Home | Ig.News</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
   )
 }
