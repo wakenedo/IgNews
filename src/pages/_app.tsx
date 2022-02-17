90cc4396e3cea21cf786bc183788f7d156f0acef
wakenedo <a_nuner@outlook.com>
2022-02-17 06:17:29 -0300

    Header Component & Assets
    App:
    Header:
    Created a header component that will be displayed at all moments of the application, all links are still static.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import {AppProps} from 'next/app'
+import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
 
 import '../styles/global.scss'
 
-function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
