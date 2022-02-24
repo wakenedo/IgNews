7602ce7f0578622c5ff1c4f1272396e26ad7e405
wakenedo <a_nuner@outlook.com>
2022-02-24 16:02:41 -0300

    Auth with NextAuth - Part 1
    integrating with Github authentication using next-auth

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,23 +1,27 @@
-import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn , useSession } from 'next-auth/client'
+
+
+import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true
+    const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             type='button'
             className={styles.signInButton}
         >
-             <FaGithub color='#04d361' />
+            <FaGithub color='#04d361' />
             wakenedo
-            <FiX color='#737380' className={styles.closeIcon}/>
+            <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type='button'
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417' />
             Sign in with Github
