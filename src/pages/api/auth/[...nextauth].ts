38b678d8f722d52b4abadbeabea35bc42c3031ea
wakenedo <a_nuner@outlook.com>
2022-02-25 20:48:28 -0300

    FaunaDb Initial Structure Done
    Set up faunadb with a simple verification to not allow multiple registers of a unique user in the database.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,9 +20,25 @@ export default NextAuth({
       )
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email: user.email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: user.email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
         return true
