6b6932110f9d79fb27a18c1bde3d84fa95e408be
wakenedo <a_nuner@outlook.com>
2022-02-28 17:07:21 -0300

    Stripe Integration - Part 2
    Setup method to don't allow duplicate users in the data base, and with the new method we're importing the function loadStripe instead of calling only stripejs. The basic structure for the subscription process is done and working

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
+import { loadStripe } from '@stripe/stripe-js'
 import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -6,23 +8,39 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession()
 
-    function handleSubscribe() {
-        const { data: session } = useSession()
+    async function handleSubscribe() {
+
         
+
         if (!session) {
             signIn('github')
             return
         }
 
         //creating check out session next/stripe
-        
+        try {
+            
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+
+            const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
+            
+            stripe.redirectToCheckout({ sessionId: sessionId })
+
+                 console.log('sessionId log:',stripe)
+        } catch (err) {
+            alert(err.message)
+        }
     }
 
     return (
         <button
             type='button'
             className={styles.subscribeButton}
+            onClick={() => handleSubscribe()}
         >
             Subscribe Now
         </button>
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,22 +1,61 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb'
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
+import { fauna } from "../../services/fauna";
 
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data : {
+        stripe_costumer_id: string;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        const session = await getSession({req});
+        const session = await getSession({ req });
 
-        const stripeCostumer = await stripe.customers.create({
-            email: session.user.email,
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        let costumerId = user.data.stripe_costumer_id
+
+        if (!costumerId) {
+            const stripeCostumer = await stripe.customers.create({
+                email: session.user.email,
+            })
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_costumer_id: stripeCostumer.id
+                        }
+                    }
+                )
+            )
+
+            costumerId = stripeCostumer.id
+        }
+
+
 
         const stripeCheckOutSession = await stripe.checkout.sessions.create({
-            customer: stripeCostumer.id,
+            customer: costumerId,
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
             line_items: [
-                { price: 'price_1KWNU1JGp1HSI7VlxZ1EaA9o', quantity : 1 }
+                { price: 'price_1KWNU1JGp1HSI7VlxZ1EaA9o', quantity: 1 }
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
@@ -25,7 +64,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         })
 
 
-        return res.status(200).json({ sessionId: stripeCheckOutSession.id})
+        return res.status(200).json({ sessionId: stripeCheckOutSession.id })
     } else {
         res.setHeader('Allow', 'POST');
         res.status(405).end('Method is not allowed')
