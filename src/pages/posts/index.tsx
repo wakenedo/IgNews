be117b33592042da5725525fa9ff397bd2c2d521
wakenedo <a_nuner@outlook.com>
2022-03-07 10:42:51 -0300

    Header Dynamic Effect
    Adapted the Header component, to display the art under the text when the user is either on the home or posts route.
    
    For that i made a component named ActiveLink to better maintain and scale the app.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,21 @@ import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string, s
+    updatedAt: string
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,21 +26,16 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Um pouco mais sobre TypeScript</strong>
-                        <p>TypeScript, desenvolvido pela Microsoft, é um superconjunto de JavaScript, o que significa que tudo que o JavaScript pode fazer, o TypeScript pode fazer (e Melhor)</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Um pouco mais sobre TypeScript</strong>
-                        <p>TypeScript, desenvolvido pela Microsoft, é um superconjunto de JavaScript, o que significa que tudo que o JavaScript pode fazer, o TypeScript pode fazer (e Melhor)</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Um pouco mais sobre TypeScript</strong>
-                        <p>TypeScript, desenvolvido pela Microsoft, é um superconjunto de JavaScript, o que significa que tudo que o JavaScript pode fazer, o TypeScript pode fazer (e Melhor)</p>
-                    </a>
+                    {posts.map((post) => {
+                        return (
+                            <a>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+
+                        );
+                    })}
                 </div>
             </main>
 
@@ -38,17 +46,28 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at('document.type', 'post')
     ], {
         fetch: ['post.title', 'post.content'],
         pageSize: 100,
     })
 
-    console.log(response)
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-br', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
 
     return {
-        props: {}
+        props: { posts }
     }
 
 }
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -4,6 +4,7 @@ export function getPrismicClient(req?: unknown,){
     const prismic = Prismic.client(
         process.env.PRISMIC_ENDPOINT,
         {
+            req,
             accessToken: process.env.PRISMIC_ACCESS_TOKEN
         }
     )
