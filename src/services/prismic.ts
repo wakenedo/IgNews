ba934dc8a924b8df36ab8f5e0f7e4ec60abe9106
wakenedo <a_nuner@outlook.com>
2022-03-07 07:27:59 -0300

    Prismic.io Setup - Part 2
    Had to downgrade Prismic.io version to ^5.1.1 instead of the more updated version, now its working properly.

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import Prismic from '@prismicio/client';
 
-export function getPrismicClient(req?: unknown){
-    const prismic = Prismic.createClient(
+export function getPrismicClient(req?: unknown,){
+    const prismic = Prismic.client(
         process.env.PRISMIC_ENDPOINT,
         {
             accessToken: process.env.PRISMIC_ACCESS_TOKEN
