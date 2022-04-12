3fa9a8cafd926295fdff317f1d3ea63e57d57e27
wakenedo <a_nuner@outlook.com>
2022-04-12 11:59:41 -0300

    Fixing Bugs
    Fixed a few type errors, the prismic-dom type issue was due to the lack of the dependency as a whole, installed it and its properly working again. But still having some bugs with react-dom/client typing or missing type wont allow the tests to run.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-    setupFilesAfterEnv: [
-        '<rootDir>/src/tests/setupTests.ts'
-    ],
+    testPathIgnorePatterns: ["/node_modules/", "/next/"],
+    setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
     transform: {
-        '^.+\\(js|jsx|ts|tsx)$':'<rootDir>/node_modules/babel-jest'
+      "^.+\\.(js|ts|jsx|tsx)$": "<rootDir>/node_modules/babel-jest" 
     },
-    testEnvironment: 'jsdom'
-}
+    testEnvironment: "jsdom",
+    moduleNameMapper: {
+      "\\.(scss|css|sass)$": "identity-obj-proxy"
+    }
+  };
